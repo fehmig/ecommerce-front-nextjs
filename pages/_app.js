1bdaddd98e79e2f79de8fdef5ba034f3d986691b
fehmiig <gunay.fehmi14@gmail.com>
2023-11-22 16:41:39 +0300

    globalStyles warning fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,24 @@
-import { createGlobalStyle } from "styled-components"
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
-  body{
-    padding:0;
-    margin:0;
+  body {
+    padding: 0;
+    margin: 0;
     font-family: 'Roboto', sans-serif;
   }
 `;
 
 
+const fontLink = `
+  @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+`;
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <style dangerouslySetInnerHTML={{ __html: fontLink }} />
       <GlobalStyles />
-       <Component {...pageProps} />
+      <Component {...pageProps} />
     </>
-   
-  )
+  );
 }

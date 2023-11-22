b2d6f283b3348571dd8de151a8f23c0c16a4efc3
fehmiig <gunay.fehmi14@gmail.com>
2023-11-22 16:22:08 +0300

    featured products done

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
 import Featured from "@/components/Featured";
 import Header from "@/components/Header";
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Product";
 
-export default function HomePage() {
+
+export default function HomePage({product}) {
   return (
     <div>
       <Header />
-      <Featured/>
+      <Featured product={product}/>
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const featuredProductId = '655de981a965346098a95765'
+  await mongooseConnect()
+  const product = await Product.findById(featuredProductId)
+  return {
+    props: {product:JSON.parse(JSON.stringify(product))},
+  }
 }
